57fc42b2819fd0133faedb32d33531f4c1d7a9ee
Wai Phyo Oo(Aron BlackJack) <96527216+waiphyoeoo@users.noreply.github.com>
2026-09-07 10:07:27 +0630

    Update version and author information in manifest

--- esp32_heart_beat/__manifest__.py
+++ esp32_heart_beat/__manifest__.py
@@ -1,15 +1,19 @@
 {
     'name': 'ESP32 IoT',
-    'version': '1.0',
+    'version': '18.0.1.0.0',
     'category': 'IoT',
     'summary': 'Control ESP32 devices from Odoo',
+
+    'author': 'BlackJack',
+    'website': 'https://yourwebsite.com',
+    'license': 'LGPL-3',
 
     'depends': [
         'base',
     ],
 
     'data': [
-        "security/ir.model.access.csv",
+        'security/ir.model.access.csv',
         'views/esp32_device_views.xml',
     ],
 
